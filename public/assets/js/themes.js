ae99559e820c8324a8a04c9ebade317e0d910971
BlackFlameDev <dawid.kowalczyk.04@gmail.com>
2025-11-19 15:59:09 +0100

    CSS tweaks, subsite tweaks, added new footer links, added RSS feed.

---
+++ public/assets/js/themes.js
@@ -0,0 +1,13 @@
+/* 
+    Handle the periodic theme changes
+
+    Right now it is an empty script, where I plan to write a function, that would check, if the system date is a specific date. 
+    For instance I'd introduce a site theme, that would activate only when it's my birthday or when it's christmas persiod (24th of december to 26th)
+
+    The script should run when the site loads.
+*/
+
+function init_theme()
+{
+    console.log("W.I.P. - Theme initiation");
+}
